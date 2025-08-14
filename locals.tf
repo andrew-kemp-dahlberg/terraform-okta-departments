3b9c338bfe7dd3c75a4a0123795bc7d9e80fcc62
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2025-08-14 11:57:05 -0500

    push

--- locals.tf
+++ locals.tf
@@ -3,20 +3,17 @@ locals {
   group_name        = "DEPT-${var.department_name}"
   group_description = "Group for the ${var.department_name} department\nManaged by Terraform. Do not edit manually."
 
+  # Extract names from the data sources (for custom attributes)
+  mailing_list_names = [for name in var.mailing_list_names : "ML-${name}"]
+  app_group_names = [for name in var.application_group_names : "APP-ROLE-${name}"]
+  push_group_names = [for name in var.push_group_names : "PG-${name}"]
+
   # Extract IDs from the data sources
   mailing_group_ids = [for grp in data.okta_group.mailing_groups : grp.id]
   app_group_ids     = [for grp in data.okta_group.application_groups : grp.id]
   push_group_ids    = [for grp in data.okta_group.push_groups : grp.id]
 
-  # Extract names from the data sources (for custom attributes)
-  # Add ML- prefix to mailing lists
-  mailing_list_names = [for grp in data.okta_group.mailing_groups : "ML-${grp.name}"]
-  
-  # Add APP-ROLE- or APP- prefix to application groups based on pattern
-  app_group_names = [for grp in data.okta_group.application_groups :  "APP-ROLE-${grp.name}" ]
 
-  # Add PG- prefix to push groups
-  push_group_names = [for grp in data.okta_group.push_groups : "PG-${grp.name}"]
 
   # Ensure Terraform management note is always included
   notes = var.notes == "Group is managed by Terraform. Do not edit manually." ? var.notes : "${var.notes}\nGroup is managed by Terraform. Do not edit manually."
